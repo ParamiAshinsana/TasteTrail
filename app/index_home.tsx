ef3edfdae891c02be4067416db7c5b636b79f054
Parami <paramiashinsana@gmail.com>
2024-07-09 14:45:10 +0530

    update

---
+++ app/index_home.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function index_home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ff8b94",
+      }}
+    >
+      <Text>Parami</Text>
+    </View>
+  );
+}
